d498f10d277c4a607ebd8b09f7be8fcd15749050
fukuda-hiroki <2311090@i-seifu.jp>
2025-09-08 10:33:14 +0900

    お前もだよ

---
+++ sugurico-nonjava/base/escape.js
@@ -0,0 +1,18 @@
+
+// XSS対策のためのエスケープ関数群
+
+/**
+ * XSS対策のためのHTMLエスケープ関数
+ */
+function escapeHTML(str) {
+    if (str === null || str === undefined) return '';
+    const s = String(str);
+    return s.replace(/[&<>"']/g, m => ({'&':'&amp;','<':'&lt;','>':'&gt;','"':'&quot;',"'":'&#39;'})[m]);
+}
+
+/**
+ * 改行を<br>に変換する関数
+ */
+function nl2br(str) {
+    return escapeHTML(str).replace(/\r\n|\n\r|\r|\n/g, '<br>');
+}
